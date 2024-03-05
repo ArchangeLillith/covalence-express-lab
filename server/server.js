1308b901b523e2574c77978af7fd9971b61936ef
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2024-03-05 10:41:00 -0500

    Finished the lab

--- server/server.js
+++ server/server.js
@@ -8,8 +8,27 @@ app.get("/", (req, res) => {
 	res.send("Hello from the web server side...");
 });
 
+//Logger
+app.use((req, res) => {
+	console.log(req.url);
+	next();
+});
+
+//Write from the form submission to the json
+app.post("/submitdata", (req, res) => {
+	fs.appendFileSync("formpost.json", `${req.body.name}\n`);
+	next();
+});
+
+//Write from the form submission to the json
+app.get("/formsubmissions", (req, res) => {
+	const fileContent = fs.readFileSync(path.join(__dirname, "formpost.json"));
+	res.send(fileContent);
+});
+
 //Static serve
 app.use(express.static(path.join(__dirname, "../public")));
 
+//Instantiate the server
 app.listen(3000);
 console.log("Listening on port 3000");
